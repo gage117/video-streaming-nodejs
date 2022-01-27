ff8c336fd3d1bce44ff25ae7d7e90636481315c2
gage117 <57643501+gage117@users.noreply.github.com>
2022-01-27 10:19:38 -0600

    Added static public folder

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 })
 
 app.get("/video", (req, res) => {
